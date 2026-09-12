1c366229b9ec72581bfa92dd601433ce8107c027
danishalvi74540-boop <danishalvi74540@gmail.com>
2026-09-12 23:57:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ def install_requirements():
 install_requirements()
 
 # 🎛️ Telegram API token (replace with your actual token)
-TOKEN = '7424479555:AAGtNAbqXax_b3b--QnGYi5_XwBbQNw'
+TOKEN = '8607329142:AAEG17Wanxy-zcrtal15Ak4QDtLPsOQt89A'
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 # 🛡️ List of authorized user IDs (replace with actual IDs)
-AUTHORIZED_USERS = [5588464519]
+AUTHORIZED_USERS = [8597047045]
 
 # 🌐 Global dictionary to keep track of user attacks
 user_attacks = {}
